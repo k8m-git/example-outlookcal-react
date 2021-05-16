b72b3cfa433d13524de1e4150d2e2450a6a0f129
k8m-git <kouki0608.job@gmail.com>
2021-05-16 18:29:24 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { Container } from 'reactstrap';
+import NavBar from './NavBar';
+import ErrorMessage from './ErrorMessage';
+import Welcome from './Welcome';
+import 'bootstrap/dist/css/bootstrap.css';
+import withAuthProvider, { AuthComponentProps } from './AuthProvider';
+import Calendar from './Calendar';
+import NewEvent from './NewEvent';
+class App extends Component<AuthComponentProps> {
+  render() {
+    let error = null;
+    if (this.props.error) {
+      error = <ErrorMessage
+        message={this.props.error.message}
+        debug={this.props.error.debug} />;
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Router>
+        <div>
+          <NavBar
+            isAuthenticated={this.props.isAuthenticated}
+            authButtonMethod={this.props.isAuthenticated ? this.props.logout : this.props.login}
+            user={this.props.user} />
+          <Container>
+            {error}
+            <Route exact path="/"
+              render={(props) =>
+                <Welcome {...props}
+                  isAuthenticated={this.props.isAuthenticated}
+                  user={this.props.user}
+                  authButtonMethod={this.props.login} />
+              } />
+            <Route exact path="/calendar"
+              render={(props) =>
+                this.props.isAuthenticated ?
+                  <Calendar {...props} /> :
+                  <Redirect to="/" />
+              } />
+            <Route exact path="/newevent"
+              render={(props) =>
+                this.props.isAuthenticated ?
+                  <NewEvent {...props} /> :
+                  <Redirect to="/" />
+              } />
+          </Container>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default withAuthProvider(App);
---
+++ src/Config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    appId: '8a24bce6-4e7f-4281-8352-089b3141259d',
+    redirectUri: 'http://localhost:3000',
+    scopes: [
+        'user.read',
+        'mailboxsettings.read',
+        'calendars.readwrite'
+    ]
+};
